02b55a78e19030f1595efc65af19719f6567d9e2
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-02-22 16:21:27 +0530

    Connected to database and integrated sales analytics with frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import router from './routes/api.route.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+    cors({
+      origin: "http://localhost:3000", 
+      methods: "GET,POST,PUT,DELETE", 
+      credentials: true,
+    })
+  );
 
 app.use(express.json());
 
