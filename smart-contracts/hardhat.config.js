3f26b75b0928082fcd92191f479b74ecea5a7ca7
midhunann <midhunmidhunan@gmail.com>
2025-02-22 16:11:51 +0530

    Deployed Smart Contract
    Solidity contracts are deployed and also can be verified with its contract address

---
+++ smart-contracts/hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+    solidity: "0.8.19",
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.ETHERSCAN_API_KEY,
+        },
+    },
+    sourcify: {
+        enabled: true
+      },
+};
